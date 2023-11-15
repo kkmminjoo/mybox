6404bf165bbf69c166ac8e9e721c162caf1bab60
kkmmi <kkmminjoo@gmail.com>
2023-11-15 21:08:37 +0900

    Init

---
+++ from_image_to_grade.js
@@ -0,0 +1,58 @@
+let grade = null; // 전역 변수 선언
+
+// 비디오 및 캔버스 요소 가져오기
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+// 웹캠 접근 함수
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+      video.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error("웹캠 접근 에러:", error);
+    });
+}
+
+// 1분마다 사진 촬영하는 함수
+setInterval(() => {
+  // 비디오의 현재 이미지를 캔버스에 그림
+  context.drawImage(video, 0, 0, 1920, 1080);
+  // 캔버스의 내용을 이미지 데이터로 변환
+  let imageData = canvas.toDataURL('image/png');
+
+  // 이미지를 Blob으로 변환하는 함수
+  async function convertToBlob(dataURL) {
+    let response = await fetch(dataURL);
+    let blob = await response.blob();
+    return blob;
+  }
+
+  // 서버에 이미지 데이터를 전송하는 함수
+  async function sendImageToServer(blob) {
+    let formData = new FormData();
+    formData.append("file", blob, "image.png");
+
+    try {
+      const response = await fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        body: formData
+      });
+      const data = await response.json();
+      console.log('성공:', data);
+      grade = data.predicted_class; // 전역 변수에 서버로부터 반환된 값을 저장
+    } catch (error) {
+      console.error('에러:', error);
+    }
+  }
+
+  // 이미지 데이터를 Blob으로 변환하고 서버에 전송
+  convertToBlob(imageData)
+    .then(blob => sendImageToServer(blob))
+    .catch(error => console.error('Blob 변환 에러:', error));
+
+}, 60000); // 60000ms = 1분
+
+
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from tensorflow.keras.models import load_model
+import numpy as np
+
+app = FastAPI()
+model = load_model("cnn3.h5")
+
+# 예측
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    # 이미지를 메모리(램)에서 직접 처리
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents))  # PIL 라이브러리 사용
+    img = img.resize((150, 150))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    class_idx = np.argmax(prediction)
+    classes = ['1', '2', '3', '4']
+    predicted_class = classes[class_idx]
+
+    return JSONResponse(content={"predicted_class": predicted_class})
+
+
+# 서버 실행
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
+
+
+
